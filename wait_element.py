d00a759b2f16b806e1e66c1088cbbf84592f831c
Sourcery AI <>
2022-06-15 08:55:12 +0000

    'Refactored by Sourcery'

--- wait_element.py
+++ wait_element.py
@@ -12,12 +12,12 @@ def wait_and_click(xpath: str, send_keys: str = None, click: bool = False) -> No
     на появившейся элемент.
     """
     wait.until(ec.element_to_be_clickable((By.XPATH, xpath)))
-    if send_keys and click:
-        elem = driver.find_element_by_xpath(xpath)
-        elem.click()
-        elem.send_keys(send_keys)
-        return None
     if send_keys:
+        if click:
+            elem = driver.find_element_by_xpath(xpath)
+            elem.click()
+            elem.send_keys(send_keys)
+            return None
         driver.find_element_by_xpath(xpath).send_keys(send_keys)
         text_el = driver.find_element_by_xpath(xpath).get_attribute("value")
         if send_keys != text_el:
@@ -30,12 +30,12 @@ def wait_and_click(xpath: str, send_keys: str = None, click: bool = False) -> No
 
 def wait_and_click_class(class_name: str, send_keys: str = None, click: bool = False) -> None:
     wait.until(ec.element_to_be_clickable((By.CLASS_NAME, class_name)))
-    if send_keys and click:
-        elem = driver.find_element_by_class_name(class_name)
-        elem.click()
-        elem.send_keys(send_keys)
-        return None
     if send_keys:
+        if click:
+            elem = driver.find_element_by_class_name(class_name)
+            elem.click()
+            elem.send_keys(send_keys)
+            return None
         driver.find_element_by_class_name(class_name).send_keys(send_keys)
         text_el = driver.find_element_by_class_name(class_name).get_attribute("value")
         if send_keys != text_el:
